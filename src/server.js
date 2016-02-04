500b0e41baec8fac5f83a76746262eee4161d447
Alex Kachura <alex.kachura@gmail.com>
2016-02-04 19:16:20 +0100

    feat(delay): added a delay for mocks CFY-4179

--- src/server.js
+++ src/server.js
@@ -9,42 +9,39 @@ var fs = require('fs');
 var _ = require('lodash');
 var url = require('url');
 
-var endpointString = process.env.CFY_ENDPOINT || 'http://10.10.1.10/'; // no need for /api/v2/, this will come as part of the backend's request from meConf.js
+var delay = process.env.CFY_DELAY || 1500; //ms
+
+var endpointString = process.env.CFY_ENDPOINT || 'http://10.10.1.10/'; //no need for /api/v2/, this will come as part of the backend's request from meConf.js
 if (endpointString[endpointString.length - 1] !== '/') {
-    endpointString = endpointString + '/'; // must end with slash;
+    endpointString = endpointString + '/'; //must end with slash;
 }
 var endpoint = url.parse(endpointString);
 logger.info('expecting to find cloudify rest at [', endpoint.href, ']');
 
-var mocksDir = path.join(__dirname, '../data','/');
-logger.info('expecting mocks json files to be in [',mocksDir,']');
+var mocksDir = path.join(__dirname, '../data', '/');
+logger.info('expecting mocks json files to be in [', mocksDir, ']');
 
 
 app.use(function findFile(req, res, next) {
-    //slicing off backend prefix
-    var reqUrl = req.url.replace('/backend/cloudify-api/','');
-    //slicing off query parameters
-    var parametersIndex = reqUrl.indexOf('?');
-    if(parametersIndex !== -1) {
-        reqUrl = reqUrl.substr(0, parametersIndex);
-    }
-    //slicing off slash if it starts with
-    if(reqUrl[0] === '/'){
-        reqUrl = reqUrl.slice(1);
-    }
+    //slicing off backend prefix, query parameters and / in the beginning
+    var reqUrl = req.url
+        .replace('/backend/cloudify-api/', '')
+        .replace(/\?.*/, '')
+        .replace(/^\//, '');
 
-    var options = [
-        mocksDir + reqUrl,
-        mocksDir + reqUrl + '.json'
-    ];
+    var path = mocksDir + reqUrl + '.json';
+    var exists;
 
-    var file = _.find(options, function (option) {
-        return fs.existsSync(option) && fs.lstatSync(option).isFile();
-    });
+    //try to find a mock file for such request
+    try {
+        exists = fs.statSync(path);
+    } catch (e) { }
 
-    if (!!file) {
-        logger.info('~~~!!! found mock file: ', file, '!!!~~~');
-        res.send(JSON.parse(fs.readFileSync(file)));
+    if (exists) {
+        logger.info('~~~!!! found mock file: ', path, '!!!~~~');
+        setTimeout(function() {
+            res.send(JSON.parse(fs.readFileSync(path)));
+        }, delay);
     } else {
         next();
     }
@@ -53,7 +50,7 @@ app.use(function findFile(req, res, next) {
 app.use(function proxyRequestToCloudify(req, res, next) {
     try {
         proxy(endpoint.host, {
-            forwardPath: function (req) {
+            forwardPath: function(req) {
                 var fPath = url.resolve(endpoint.path, url.parse(req.url).path.substring(1));
                 logger.info('forwarding request to', fPath);
                 return fPath;
@@ -70,7 +67,7 @@ app.use(function error(errorObj, req, res/*, next */) {
     res.status(500).send({'key': 'proxy_error', stack: errorObj.stack.split('\n'), reason: errorObj.toString()});
 });
 
-var server = app.listen(3333, function () {
+var server = app.listen(3333, function() {
     var host = server.address().address;
     var port = server.address().port;
 
